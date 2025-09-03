e3bf91ec343eaca300ea99d3524470d139de7e06
Adam O'Neil <adamosoftware@gmail.com>
2025-09-02 21:24:25 -0400

    Add project files.

---
+++ ApiKeys.Service/ApiKeyAuthorizationHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+
+namespace ApiKeys.Service;
+
+public class ApiKeyRequirement : IAuthorizationRequirement { }
+
+public class ApiKeyAuthorizationHandler(ApiKeyManager apiKeyManager, IHttpContextAccessor httpContextAccessor) : AuthorizationHandler<ApiKeyRequirement>
+{
+	private readonly ApiKeyManager _manager = apiKeyManager;
+	private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+
+	protected override async Task HandleRequirementAsync(
+		AuthorizationHandlerContext context,
+		ApiKeyRequirement requirement)
+	{
+		var httpContext = _httpContextAccessor.HttpContext;
+		if (httpContext == null) return;
+
+		if (httpContext.Request.Headers.TryGetValue("ApiKey", out var apiKey) && !string.IsNullOrWhiteSpace(apiKey))
+		{
+			if (await _manager.ValidateAsync(apiKey!))
+			{
+				context.Succeed(requirement);
+			}
+		}
+	}
+}
---
+++ ApiKeys.Service/ApiKeyManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ApiKeys.Service;
+
+public abstract class ApiKeyManager
+{
+	protected abstract Task StoreHashAsync(string name, string hash);
+	protected abstract Task<string> GetStoredHashAsync(string base64Hash);
+	protected abstract string Salt { get; }
+
+	public async Task<string> GenerateAsync(string name)
+	{
+		var keyBytes = new byte[32];
+		RandomNumberGenerator.Fill(keyBytes);
+		var apiKey = Convert.ToBase64String(keyBytes);
+
+		// Hash the key
+		var hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(Salt + apiKey));
+		var hash = Convert.ToBase64String(hashBytes);
+		
+		await StoreHashAsync(name, hash);
+
+		// client should see only once and store it securely
+		return apiKey;
+	}
+
+	public async Task<bool> ValidateAsync(string apiKey)
+	{
+		var hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(Salt + apiKey));
+		var hash = Convert.ToBase64String(hashBytes);
+		var storedHash = await GetStoredHashAsync(hash);
+		return storedHash == hash;
+	}
+}
